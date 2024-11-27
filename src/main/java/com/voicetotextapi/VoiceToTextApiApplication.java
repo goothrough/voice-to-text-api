2e9d59c1221432d414e07d7d86990b525f5a2c81
Suguru Ohashi <goothrough315@gmail.com>
2024-11-27 10:39:55 -0800

    first commit

---
+++ src/main/java/com/voicetotextapi/VoiceToTextApiApplication.java
@@ -0,0 +1,13 @@
+package com.voicetotextapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceToTextApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceToTextApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voicetotextapi/VoiceToTextApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voicetotextapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceToTextApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
