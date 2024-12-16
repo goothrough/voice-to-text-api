c6af732ac99a84c6d62dbc9c241a2800c6efc49f
Suguru Ohashi <goothrough315@gmail.com>
2024-12-15 21:11:45 -0800

    add database maintainance scheduler

--- src/main/java/com/voicetotextapi/VoiceToTextApiApplication.java
+++ src/main/java/com/voicetotextapi/VoiceToTextApiApplication.java
@@ -2,8 +2,10 @@ package com.voicetotextapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VoiceToTextApiApplication {
 
 	public static void main(String[] args) {
